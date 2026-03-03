bd272af59203a98ac83a5ab035a5bb55ab1625f4
kunal734 <kunal.pragati73@gmail.com>
2026-03-04 04:30:37 +0530

    Initial commit - Tic-Tac-Toe project

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+// // postcss.config.js
+// module.exports = {
+//   plugins: [
+//     require('tailwindcss'),
+//     require('autoprefixer'),
+//   ],
+// };
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // adjust these globs to match your project
+    './src/**/*.{js,jsx,ts,tsx,html}',
+    // './public/index.html'           // if you use an html file
+  ],
+  theme: {
+    extend: {},
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
